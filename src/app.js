83fc2208436e7f1eb5f07724a684e8d323c26ea6
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-04-13 18:38:07 +1000

    Testing-supertest-changing

---
+++ src/app.js
@@ -0,0 +1,36 @@
+// 2. Import Congifuration
+const config = require("./utils/config");
+const express = require("express");
+// 4. helmet - for security
+const helmet = require("helmet");
+// 6. CORS - for cross-origin requests
+const cors = require("cors");
+// 5. morgan - for logging
+const morgan = require("./middleware/morgan.middleware");
+// 7. rateLimit - for limiting requests
+const rateLimit = require("./middleware/rateLimit.middleware");
+const errorMiddleware = require("./middleware/error/index");
+// 3. Setting Logger - winston - pay attention to the destructuring cus there are two objects in the module.exports
+const { logger } = require("./utils/logger");
+const v1Router = require("./routes");
+const { connect } = require("mongoose");
+const connectToDb = require("./utils/db");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan);
+app.use(rateLimit);
+app.use(express.json()); // Make sure to the body is parsed as JSON
+
+// 1. Confirm the server is running
+// app.get("/", (req, res) => {
+//     res.json("Hello, world!");
+// })
+// 8. Replace the above to Router
+app.use("/v1", v1Router);
+// 10. Error Handling Middleware
+errorMiddleware.forEach(handler => app.use(handler));
+
+module.exports = app;
--- tests/unit/authGuard.middleware.test.js
+++ tests/unit/authGuard.middleware.test.js
@@ -1,9 +1,12 @@
 jest.mock('../../src/utils/config', () => ({}));
+jest.mock('../../src/utils/jwt');
+
 const authGuardMiddleware = require("../../src/middleware/authGuard.middleware")
+const { validateToken } = require("../../src/utils/jwt");
 
 describe("auth guard middleware", () => {
+    // (1) Test for missing token
     it("should return 401 if authorization header is not defined", () => {
-
         // 01 setup
         const req = { header: jest.fn() };
         const res = {
@@ -13,13 +16,31 @@ describe("auth guard middleware", () => {
         const next = jest.fn();
         // Cus status().json() is a chain function, so we need to mock it, if not, it will just return undefined
         res.status.mockReturnValue(res);
-
         // 02 execute
         authGuardMiddleware(req, res, next);
-
         // 03 compare - assertion
         expect(res.status).toHaveBeenCalledWith(401);
         expect(res.json).toHaveBeenCalled();
         expect(next).not.toHaveBeenCalled();
+    })
+
+    // (2) Test for calling next() when token is valid
+    it("should pass the authentication check", () => {
+        // 01 setup
+        const req = { header: jest.fn()};
+        const res = {};
+        const next = jest.fn();
+        const payload = { id: 123 };
+        const token = 'token';
+        // Mock the header function to return a valid token
+        req.header.mockReturnValue(`Bearer ${token}`);
+        // Mock a validateToken function to return a payload
+        validateToken.mockReturnValue(payload);
+        // 02 execute
+        authGuardMiddleware(req, res, next);
+        // 03 compare
+        expect(validateToken).toHaveBeenCalledWith(token);
+        expect(req.user).toEqual(payload);
+        expect(next).toHaveBeenCalled();
     })
 })
